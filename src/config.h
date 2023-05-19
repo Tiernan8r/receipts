35adfb4ecfe03ff025a75acd37a1560b591322cc
Tiernan8r <tiernan8r@pm.me>
2023-05-19 21:54:46 +0100

    config.h

---
+++ src/config.h
@@ -0,0 +1,28 @@
+/*
+    Copyright (C) 2023  Tiernan8r
+    Fork of origin simple-scan at https://gitlab.gnome.org/GNOME/simple-scan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <string>
+
+const std::string PKGDATADIR;
+const std::string LOCALE_DIR;
+const std::string GETTEXT_PACKAGE;
+const std::string VERSION;
+
+#endif // CONFIG_H
