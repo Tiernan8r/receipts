48d6ccfe39c5a587db56bf534652294623bfa280
Tiernan8r <tiernan8r@pm.me>
2023-06-04 18:23:03 +0100

    WIP on autosave_manager

--- src/autosave_manager.h
+++ src/autosave_manager.h
@@ -1,4 +1,4 @@
-/* 
+/*
     Copyright (C) 2023  Tiernan8r
     Fork of origin simple-scan at https://gitlab.gnome.org/GNOME/simple-scan
 
@@ -20,71 +20,47 @@
 
 #include <string>
 #include <map>
+#include <glibmm-2.68/glibmm.h>
+#include <sigc++-3.0/sigc++/sigc++.h>
 #include "page.h"
 
 class AutosaveManager
 {
-    private:
-        static std::string AUTOSAVE_DIR = Path.build_filename (Environment.get_user_cache_dir (), "receipts", "autosaves");
-        static std::string AUTOSAVE_FILENAME = "autosave.book";
-        static std::string AUTOSAVE_PATH = Path.build_filename (AUTOSAVE_DIR, AUTOSAVE_FILENAME);
+private:
+    static std::string AUTOSAVE_DIR = Path.build_filename(Environment.get_user_cache_dir(), "receipts", "autosaves");
+    static std::string AUTOSAVE_FILENAME = "autosave.book";
+    static std::string AUTOSAVE_PATH = Path.build_filename(AUTOSAVE_DIR, AUTOSAVE_FILENAME);
 
-        uint update_timeout = 0;
+    uint update_timeout = 0;
 
-        std::map<Page, std::string> page_filenames;
+    std::map<Page, std::string> page_filenames;
 
-        Book *book_ = NULL;
+    Book *book_ = NULL;
+    sigc::connection page_added_connection;
+    sigc::connection page_removed_connection;
+    sigc::connection reordered_connection;
+    sigc::connection cleared_connection;
 
-        std::string get_value (KeyFile file, std::string group_name, std::string key, std::string default = "");
-        int get_integer (KeyFile file, std::string group_name, std::string key, int default = 0);
-        bool get_boolean (KeyFile file, std::string group_name, std::string key, bool default = false);
-        void save (bool do_timeout = true);
-        void real_save (void);
-        void save_pixels (Page page);
+    std::string get_value(Glib::KeyFile file, std::string group_name, std::string key, std::string& default);
+    int get_integer(Glib::KeyFile file, std::string group_name, std::string key, int& default);
+    bool get_boolean(Glib::KeyFile file, std::string group_name, std::string key, bool& default);
+    void save(bool do_timeout = true);
+    void real_save(void);
+    void save_pixels(Page page);
 
-    public:
-        Book book {
-        get
-        {
-            return book_;
-        }
-        set
-        {
-            if (book_ != null)
-            {
-                for (var i = 0; i < book_.n_pages; i++)
-                {
-                    var page = book_.get_page (i);
-                    on_page_removed (page);
-                }
-                book_.page_added.disconnect (on_page_added);
-                book_.page_removed.disconnect (on_page_removed);
-                book_.reordered.disconnect (on_changed);
-                book_.cleared.disconnect (on_cleared);
-            }
-            book_ = value;
-            book_.page_added.connect (on_page_added);
-            book_.page_removed.connect (on_page_removed);
-            book_.reordered.connect (on_changed);
-            book_.cleared.connect (on_cleared);
-            for (var i = 0; i < book_.n_pages; i++)
-            {
-                var page = book_.get_page (i);
-                on_page_added (page);
-            }
-        }
-    };
+public:
+    Book* get_book(void);
+    void set_book(Book *book);
 
-    AutosaveManager (void);
-    bool exists (void);
-    void load (void);
-    void cleanup (void);
-    void on_page_added (Page page);
-    void on_page_removed (Page page);
-    void on_scan_finished (Page page);
-    void on_changed (void);
-    void on_cleared (void);
+    AutosaveManager(void);
+    bool exists(void);
+    void load(void);
+    void cleanup(void);
+    void on_page_added(Page page);
+    void on_page_removed(Page page);
+    void on_scan_finished(Page page);
+    void on_changed(void);
+    void on_cleared(void);
 };
 
-
-#endif //AUTOSAVE_MANAGER_H
+#endif // AUTOSAVE_MANAGER_H
